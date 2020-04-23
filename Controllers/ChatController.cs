10ed7ad7244f39a952909e956b955a23aa3321f9
selser1998 <57316104+selser1998@users.noreply.github.com>
2020-04-23 22:15:28 +0300

    add

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSocketChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("InsertUserName");
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            return View("Index", username);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSocketChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
